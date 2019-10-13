0587d7df6e8f4ad067511eae2f67f612ef144695
Amin Hosseini <amin.hoseiny@gmail.com>
2019-10-13 23:56:05 +0330

    Add Timer to product on sale with dates

--- index.php
+++ index.php
@@ -21,14 +21,33 @@ Class wc_onsale_product {
 
     function woocommerce_custom_badge( $output_html, $post, $product ) {
 
+        add_filter( 'woocommerce_single_product_summary', [$this,'woocommerce_onsale_timer']);
+
         // Added compatibility with WC +3
         $regular_price = method_exists( $product, 'get_regular_price' ) ? $product->get_regular_price() : $product->regular_price;
         $sale_price = method_exists( $product, 'get_sale_price' ) ? $product->get_sale_price() : $product->sale_price;
-
         $saved_price = wc_price( $regular_price - $sale_price );
+
         $output_html .=' '.'<span class="onsale">' . esc_html__( 'Save', 'woocommerce' ) . ' ' . $saved_price .'</span>';
 
         return $output_html;
     }
+
+    function woocommerce_onsale_timer ( ) {
+        global $product;
+        $saledate = $product->get_date_on_sale_to();
+        if ($saledate == null ) return;
+        wp_enqueue_script('woo-On-Sale',plugin_dir_url( __FILE__ ).'onsale.js');
+        $today = new DateTime();
+        $diff = $today->diff($saledate);
+        $output_html = '<div class="onSaleFlash">';
+        $output_html .=' '.'<span class="onsale"><span id="Day">' . $diff->d .'</span> Days '.'</span>';
+        $output_html .=' '.'<span class="onsale"><span id="Hour">' . $diff->h .'</span> Hours '.'</span>';
+        $output_html .=' '.'<span class="onsale"><span id="Min">' . $diff->i .'</span> Minutes '.'</span>';
+        $output_html .=' '.'<span class="onsale"><span id="Sec">' . $diff->s .'</span> Seconds '.'</span>';
+        $output_html .='</div>';
+        echo $output_html;
+    }
+
 }
 new wc_onsale_product;
